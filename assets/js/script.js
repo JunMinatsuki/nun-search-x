5fc82396d15ca4645ccbab96ef85a2ae4a8b3399
Jun Minatsuki <jumpei.sawada@me.com>
2025-05-25 20:37:18 +0900

    告知欄表示（テスト）

--- assets/js/script.js
+++ assets/js/script.js
@@ -226,3 +226,41 @@ window.addEventListener('DOMContentLoaded', () => {
     floatingNunnunSwitcher(calcScrollEnd(scrollEndOffset));
   })
 })
+
+// X埋め込み テーマ切り替え
+const darkmode = window.matchMedia('(prefers-color-scheme: dark)');
+function loadWidgets(){
+  const embed = document.querySelectorAll('blockquote.twitter-tweet');
+  if(embed.length === 0){
+    return
+  }
+  for(let i = 0; i < embed.length; i++){
+    if(darkmode.matches){
+      embed[i].setAttribute('data-theme', 'dark');
+      } else {
+      embed[i].setAttribute('data-theme', 'light'); 
+    }
+    embed[i].setAttribute('data-width', '550');
+    embed[i].setAttribute('data-align', 'center');
+  }
+  const script = document.createElement('script');
+  script.src = "https://platform.twitter.com/widgets.js";
+  document.body.appendChild(script);
+}
+  
+function changeEmbedX(){
+  const iframe = document.querySelectorAll('div.twitter-tweet-rendered iframe');
+  if(iframe.length === 0){
+    return
+  }
+  for(let i = 0; i < iframe.length; i++){
+    if(darkmode.matches){
+      iframe[i].src = iframe[i].src.replace('&theme=light&', '&theme=dark&');
+    } else {
+      iframe[i].src = iframe[i].src.replace('&theme=dark&', '&theme=light&');
+    }
+  }
+}
+
+window.addEventListener('DOMContentLoaded', loadWidgets);
+darkmode.addEventListener('change', changeEmbedX);
